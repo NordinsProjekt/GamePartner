8d0c458f0f9d5b62df707ef1207a6aff58afc8f4
Markus Nordin <belfegorc4@gmail.com>
2023-01-21 22:29:34 +0100

    Added Isunder Rule for Int and Decimal

---
+++ DomainValidation/IsUnder_RuleTestingValidating.cs
@@ -0,0 +1,55 @@
+﻿using MtGDomain.Rules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainValidation
+{
+    [Trait("Domain", "IsUnder Rule Validating")]
+    public class IsUnder_RuleTestingValidating
+    {
+        [Fact]
+        public void IsUnderINT_SettingFive_ValueIsFour_ShouldReturnTrue()
+        {
+            IsUnder isUnder = new(5);
+            Assert.True(isUnder.Validate(4));
+        }
+
+        [Fact]
+        public void IsUnderINT_SettingFive_ValueIsFive_ShouldReturnFalse()
+        {
+            IsUnder isUnder = new(5);
+            Assert.False(isUnder.Validate(5));
+        }
+
+        [Fact]
+        public void IsUnderINT_SettingFive_ValueIsSix_ShouldReturnFalse()
+        {
+            IsUnder isUnder = new(5);
+            Assert.False(isUnder.Validate(6));
+        }
+
+        [Fact]
+        public void IsUnderDECIMAL_SettingFivePoint5_ValueIsFour_ShouldReturnTrue()
+        {
+            IsUnder isUnder = new((decimal)5.5);
+            Assert.True(isUnder.Validate((decimal)4));
+        }
+
+        [Fact]
+        public void IsUnderDECIMAL_SettingFivePoint5_ValueIsFIVE_ShouldReturnTrue()
+        {
+            IsUnder isUnder = new((decimal)5.5);
+            Assert.True(isUnder.Validate((decimal)5));
+        }
+
+        [Fact]
+        public void IsUnderDECIMAL_SettingFivePoint5_ValueIssIX_ShouldReturnFalse()
+        {
+            IsUnder isUnder = new((decimal)5.5);
+            Assert.False(isUnder.Validate((decimal)6));
+        }
+    }
+}
---
+++ MtGDomain/Rules/IsUnder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MtGDomain.Rules
+{
+    public class IsUnder : IRequirement
+    {
+        private object Value { get; set; }
+        public IsUnder(int value) { Value = value; }
+        public IsUnder(decimal value) { Value = value; }
+
+        public bool Validate(object obj)
+        {
+            if (obj.GetType() == typeof(int))
+                return (int)Value > (int)obj;
+            if (obj.GetType() == typeof(decimal))
+                return (decimal)Value > (decimal)obj;
+            return false;
+        }
+    }
+}

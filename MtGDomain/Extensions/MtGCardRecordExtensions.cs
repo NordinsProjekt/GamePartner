4ebfb64a217c67f76b3818744f4aadd051755821
Markus Nordin <belfegorc4@gmail.com>
2023-07-20 10:28:35 +0200

    dwq

---
+++ MtGDomain/Extensions/MtGCardRecordExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Domain.MtGDomain.DTO;
+using MtGDomain.Constants;
+using MtGDomain.Enums;
+
+namespace MtGDomain.Extensions
+{
+    public static class MtGCardRecordExtensions
+    {
+        public static bool DoesCardHaveThisColor(this MtGCardRecordDTO card, MtGColor color)
+        {
+            switch(color)
+            {
+                case MtGColor.Black:
+                    return card.ManaCost.Contains(MtGColorAsChar.Black);
+                case MtGColor.White:
+                    return card.ManaCost.Contains(MtGColorAsChar.White);
+                case MtGColor.Red:
+                    return card.ManaCost.Contains(MtGColorAsChar.Red);
+                case MtGColor.Green:
+                    return card.ManaCost.Contains(MtGColorAsChar.Green);
+                case MtGColor.Blue:
+                    return card.ManaCost.Contains(MtGColorAsChar.Blue);
+
+                default: return false;
+            }
+        }
+    }
+}
---
+++ UnitTestForSolution/0.3 Application/MtGQuizServiceTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestForSolution._0._3_Application
+{
+    [Trait("Application", "MtGQuizService")]
+    public class MtGQuizServiceTests
+    {
+
+    }
+}

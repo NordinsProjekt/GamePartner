ff3c7c48c9f6bd7554e277e00aa80dddf785baf7
Markus Nordin <belfegorc4@gmail.com>
2023-07-22 06:21:29 +0200

    Added FilterLogic

---
+++ MtGCard_Service/DTO/MtGSearchFilter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MtGCard_Service.DTO
+{
+    public class MtGSearchFilter
+    {
+        public bool Creature { get; set; }
+        public bool Enchantment { get; set; }
+        public bool Instant { get; set; }
+        public bool Sorcery { get; set; }
+        public bool Artefact { get; set; }
+        public bool Battle { get; set; }
+        public bool Planeswalker { get; set; }
+        public bool Land { get; set; }
+    }
+}

c0b53e6fd7d634818f73a343023bbafbea29354c
Markus Nordin <belfegorc4@gmail.com>
2023-07-29 23:53:10 +0200

    Quiz Autofocus

--- MtGDomain/Hashmaps/MtGSets.cs
+++ MtGDomain/Hashmaps/MtGSets.cs
@@ -5,15 +5,19 @@ namespace MtGDomain.Hashmaps
     {
         public static readonly Dictionary<string, string > Values = new Dictionary<string, string>
         {
+            { "The Brothers' War", "BRO" },
             { "Dominaria United", "DMU" },
             { "Innistrad: Midnight Hunt", "MID" },
+            { "Kaldheim", "KHM" },
             { "Eldritch Moon", "EMN" },
             { "Magic Origins", "ORI" },
             { "Avacyn Restored", "AVR" },
             { "Dark Ascension", "DKA" },
             { "Innistrad", "ISD" },
             { "New Phyrexian", "NPH" },
-            { "The Dark", "DRK" }
+            { "Chronicles", "CHR" },
+            { "The Dark", "DRK" },
+            { "Unlimited", "2ED" }
         };
     }
 }

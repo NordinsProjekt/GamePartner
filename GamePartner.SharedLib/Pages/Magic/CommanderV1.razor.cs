1d8f049e8f109e14d0c687d9c08ffedfbc0523f9
Markus Nordin <belfegorc4@gmail.com>
2023-10-08 21:18:23 +0200

    Added all files Fixed namespaces Added usings

---
+++ GamePartner.SharedLib/Pages/Magic/CommanderV1.razor.cs
@@ -0,0 +1,65 @@
+﻿using ApplicationLayer.MtGCard_Service.DTO;
+using ApplicationLayer.MtGCard_Service;
+using Microsoft.AspNetCore.Components;
+using ApplicationLayer.MtGCard_Service.Interface;
+using Microsoft.JSInterop;
+using System.Runtime.InteropServices;
+using MtGCard_Service.DTO;
+using MtGCard_Service;
+
+using Domain.MtGDomain.DTO;
+
+namespace GamePartner.SharedLib.Pages.Magic
+{
+    public partial class CommanderV1
+    {
+        [Inject]
+        IMtGCardRepository Rep { get; set; }
+        [Inject]
+        IJSRuntime JsRuntime { get; set; }
+        List<MtGCardRecordDTO> searchResult = new List<MtGCardRecordDTO>();
+        MtGCardRecordDTO? clickedCard;
+        public List<MtGPlayerLife_DTO> playerList = new List<MtGPlayerLife_DTO>();
+
+        public int playerId { get; set; }
+        string? SearchText { get; set; }
+
+        protected override void OnInitialized()
+        {
+            for (int i = 0; i < 4; i++)
+                playerList.Add(MtGPlayerService.MakeNewPlayer(i, "Player " + (i + 1), 40, 0, null));
+        }
+        public async void Search()
+        {
+            MtGCardService mtg = new MtGCardService(Rep);
+            if (SearchText != "")
+            {
+                searchResult = await mtg.GetCardByName(SearchText);
+                clickedCard = default(MtGCardRecordDTO);
+                StateHasChanged();
+            }
+        }
+
+        public async void ShowCard(string id)
+        {
+            clickedCard = searchResult.Where(x => x.Id == id).FirstOrDefault();
+            await JsRuntime.InvokeVoidAsync("OnScrollEvent");
+            StateHasChanged();
+        }
+
+        public void SaveCard()
+        {
+            if (clickedCard != null && playerId > 3 == false && 0 > playerId == false)
+            {
+                playerList[playerId].CardList.Add(clickedCard);
+            }
+        }
+        public void DeleteCard(string cardId, int playerId)
+        {
+            var card = playerList[playerId].CardList.Where(x => x.Id == cardId).FirstOrDefault();
+            if (card != null)
+                playerList[playerId].CardList.Remove(card);
+            StateHasChanged();
+        }
+    }
+}
---
+++ HybridMauiClient/wwwroot/js/drawCounter.js
@@ -0,0 +1,25 @@
+﻿function DrawLifeCounter(life,window) {
+    var canvas = document.getElementById(window);
+    var ctx = canvas.getContext("2d");
+    //Start
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, 200, 200);
+    ctx.font = '116px serif';
+    ctx.fillStyle = "black";
+    ctx.textAlign = 'center';
+    ctx.textBaseline = "middle";
+    ctx.fillText(life, 100, 100);
+}
+
+function DrawPoisonCounter(poison, window) {
+    var canvas = document.getElementById(window);
+    var ctx = canvas.getContext("2d");
+    //Start
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, 200, 200);
+    ctx.font = '116px serif';
+    ctx.fillStyle = "green";
+    ctx.textAlign = 'center';
+    ctx.textBaseline = "middle";
+    ctx.fillText(poison, 100, 100);
+}

49278e7c138fc997e5b7a042ca17297eb55b571b
Markus Nordin <belfegorc4@gmail.com>
2023-11-12 08:59:01 +0100

    Adding controller

---
+++ Portal.Api/Controllers/MagicSetController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MtGDomain.DTO;
+
+namespace Portal.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MagicSetController : Controller
+    {
+        [HttpGet("GetByCode")]
+        public IActionResult Get()
+        {
+            return View();
+        }
+
+        [HttpGet("GetCardsWithFilter")]
+        public IActionResult Get(MtGSearchFilterRecord filter)
+        {
+            return View();
+        }
+    }
+}
